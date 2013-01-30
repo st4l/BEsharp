c483e3027aa826dcfff072420acf371a205ca800
St4l <stalidon@gmail.com>
2013-01-30 06:32:08 -0300

    Moved Log methods to ILog ext methods

---
+++ besharp/LogExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net;
+using log4net.Core;
+
+namespace BESharp
+{
+    public static class LogExtensions
+    {
+        [Conditional("TRACE")]
+        public static void Trace(this ILog logger, string msg)
+        {
+            logger.Logger.Log(
+                logger.GetType(),
+                Level.Debug, // Level.Trace
+                msg,
+                null);
+        }
+
+
+        [Conditional("TRACE")]
+        public static void TraceFormat(this ILog logger, string fmt, params object[] args)
+        {
+            logger.Logger.Log(
+                logger.GetType(),
+                Level.Debug, // Level.Trace
+                string.Format(CultureInfo.InvariantCulture, fmt, args),
+                null);
+        }
+
+
+
+    }
+}
